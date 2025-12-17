a478ab5283b3a2ee082b4483b84129d0a06f63b0
houra <dhourda@gmail.com>
2025-12-17 10:54:48 +0100

    fix logg, add documentation

--- Logger.py
+++ Logger.py
@@ -4,10 +4,13 @@ import os.path
 
 
 class Logger:
+    """
+    Class that handles logging. Writes log messages to both console and specified log file
+    """
     def __init__(self,path_file):
         """
-
-        :param path_file:
+        Inicialize logging and check if path to file exists, if not it creates a new one
+        :param path_file: path to logging file example 'logs/app.log'
         """
         self.path = None
 
@@ -26,9 +29,14 @@ class Logger:
             self.path  = None
 
 
-
-
     def _write_log(self,type,message):
+        """
+        Method to handle formatting and writing of logs
+        it formats timestamp, prints message to console and appends it to log file (if exists)
+
+        :param type: str type of log (INFORMATION/ERROR....)
+        :param message: str message to log
+        """
         try:
             time = datetime.datetime.now().strftime("%H:%M:%S")
 
@@ -42,8 +50,17 @@ class Logger:
         except Exception as e:
             print(f"LOGGER WRITE FAILURE: Cant write into file: {e}")
 
+
     def write_info(self,message):
+        """
+        Logs an informational message
+        :param message: information to log
+        """
         self._write_log("INFO", message)
 
     def write_error(self,message):
+        """
+        Logs an Error message
+        :param message: error detail;s to log
+        """
         self._write_log("ERROR",message)
