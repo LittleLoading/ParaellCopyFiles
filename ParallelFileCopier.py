1c21f1e387990604fbe8e823aa4ce5e0e5e61e4c
houra <dhourda@gmail.com>
2025-12-18 13:43:46 +0100

    processes instead of threads

--- ParallelFileCopier.py
+++ ParallelFileCopier.py
@@ -1,7 +1,7 @@
 import os
 import queue
 import shutil
-import threading
+import multiprocessing
 import time
 
 
@@ -11,15 +11,15 @@ from Logger import Logger
 class ParallelFileCopier:
     """
         A class used to copy files recursively from a source directory to a destination
-        directory using multiple threads for improved performance.
+        directory using multiple PROCESSES for improved performance.
         """
 
-    def __init__(self, source_dir: str, destination_dir: str, number_of_threads: int, logger: Logger):
+    def __init__(self, source_dir: str, destination_dir: str, number_of_processes: int, logger: Logger):
         """
-        Initializes the ParallelFileCopier with source/destination paths and thread configuration
+        Initializes the ParallelFileCopier with source/destination paths and processes configuration
             :param source_dir (str): The path to the source directory containing files to copy
             :param destination_dir (str): The path where files should be copied to
-            :param number_of_threads (int): The number of concurrent worker threads to spawn
+            :param number_of_processes (int): The number of concurrent worker processes to spawn
             """
         if not isinstance(source_dir, str):
             raise TypeError(f"Argument 'source_dir' must be a string")
@@ -27,16 +27,16 @@ class ParallelFileCopier:
         if not isinstance(destination_dir, str):
             raise TypeError(f"Argument 'destination_dir' must be a string")
 
-        if not isinstance(number_of_threads, int):
-            raise TypeError(f"Argument 'number_of_threads' must be an integer")
+        if not isinstance(number_of_processes, int):
+            raise TypeError(f"Argument 'number_of_processes' must be an integer")
 
-        if number_of_threads < 1:
-            raise ValueError(f"Number of threads must be at least 1, got {number_of_threads}.")
+        if number_of_processes < 1:
+            raise ValueError(f"Number of processes must be at least 1, got {number_of_processes}.")
 
 
         self.source_dir = source_dir
         self.destination_dir = destination_dir
-        self.number_of_threads = number_of_threads
+        self.number_of_processes = number_of_processes
         self.logger = logger
 
         if not os.path.exists(self.source_dir):
@@ -45,7 +45,7 @@ class ParallelFileCopier:
         if not os.path.isdir(self.source_dir):
             raise NotADirectoryError(f"Source path is not a directory")
 
-        self.work_queue = queue.Queue()
+        self.work_queue = multiprocessing.JoinableQueue()
         self.files_count = 0
         self.total_bytes = 0
 
@@ -57,7 +57,7 @@ class ParallelFileCopier:
         of files found, and populates the work_queue with tuples of (source_path, dest_path).
 
         After all files are indexed, it inserts 'None' (poison pills) into the queue
-        equal to the number of threads, signalling the consumers to terminate.
+        equal to the number of processes, signalling the consumers to terminate.
         """
         file_count = 0
         total_bytes = 0
@@ -85,10 +85,10 @@ class ParallelFileCopier:
         #print(f"[Producer] Found {file_count} files. Total size: {readable_size}")
         self.logger.write_info(f"[Producer] Found {file_count} files. Total size: {readable_size}")
 
-        for every_thread in range(self.number_of_threads):
+        for every_process in range(self.number_of_processes):
             self.work_queue.put(None)
 
-    def _file_consumer(self, thread_id: int):
+    def _file_consumer(self, process_id: int):
         """
         Internal method acting as the Consumer (Worker).
 
@@ -98,7 +98,7 @@ class ParallelFileCopier:
 
         The loop terminates when it encounters 'None' in the queue.
 
-        :param thread_id (int): A unique identifier for the thread (used for logging).
+        :param process_id (int): A unique identifier for the process (used for logging).
         """
         while True:
             task = self.work_queue.get()
@@ -117,42 +117,42 @@ class ParallelFileCopier:
                 shutil.copy2(src_path, dest_path)
 
             except Exception as e:
-                #print(f"[Worker {thread_id}] ERROR copying {src_path}: {e}")
-                self.logger.write_error(f"[Worker {thread_id}] ERROR copying {src_path}: {e}")
+                #print(f"[Worker {process_id}] ERROR copying {src_path}: {e}")
+                self.logger.write_error(f"[Worker {process_id}] ERROR copying {src_path}: {e}")
             self.work_queue.task_done()
 
     def start_copying(self):
         """
         Manages the copying process:
 
-        Start X number of Consumer Threads
-        Starts Producer thread to index files
+        Start X number of Consumer Processes
+        Starts Producer Process to index files
         waits for producer to finish
         waits for queue to be empty
-        waits for all consumer threads to terminate
+        waits for all consumer process to terminate
         prints statistics and duration
         """
         start_time = time.time()
-        threads = []
+        processes = []
 
-       # print(f"Starting {self.number_of_threads} Worker Threads...")
-        self.logger.write_info(f"Starting {self.number_of_threads} Worker Threads...")
+       # print(f"Starting {self.number_of_processes} Worker Processes...")
+        self.logger.write_info(f"Starting {self.number_of_processes} Worker Processes...")
 
-        for i in range(self.number_of_threads):
-            t = threading.Thread(target=self._file_consumer, args=(i + 1,))
-            t.start()
-            threads.append(t)
+        for i in range(self.number_of_processes):
+            p = multiprocessing.Process(target=self._file_consumer, args=(i + 1,))
+            p.start()
+            processes.append(p)
 
-        #print("Starting Producer Thread...")
-        self.logger.write_info("Starting Producer Thread...")
-        producer_thread = threading.Thread(target=self._file_producer)
-        producer_thread.start()
+        #print("Starting Producer Process...")
+        self.logger.write_info("Starting Producer Process...")
+        producer_proces = multiprocessing.Process(target=self._file_producer)
+        producer_proces.start()
 
-        producer_thread.join()
+        producer_proces.join()
         self.work_queue.join()
 
-        for t in threads:
-            t.join()
+        for p in processes:
+            p.join()
 
         end_time = time.time()
         duration = end_time - start_time
